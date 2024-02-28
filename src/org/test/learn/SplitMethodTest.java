2884adff10eb8d1ec28b4393ddfa797261c24d42
Balaji Ravichandran <balajicrvino@gamil.com>
2024-02-28 21:14:01 +0530

    String methods

---
+++ src/org/test/learn/SplitMethodTest.java
@@ -0,0 +1,34 @@
+package org.test.learn;
+
+public class SplitMethodTest {
+
+	public static void main(String[] args) {
+		String s1 = "Hai Welcome to Java Class at 8PM Scheduled";
+
+		// hai-1
+		// welcome-1
+		// to-2
+		// java-3
+		// class-4
+		// at-5
+		// 8-PM-7
+		// Scheduled-8
+
+		String[] s2Array = s1.split("Hai");
+//
+//		System.out.println(s2.length);
+
+//		System.out.println(s1);
+
+//		for (int i = 0; i < s2.length; i++) {
+//			System.out.println(s2[i]);
+//		}
+		
+		for(String x:s2Array)
+		{
+			System.out.println(x);
+		}
+
+	}
+
+}
--- src/org/test/learn/StringMethodPractice.java
+++ src/org/test/learn/StringMethodPractice.java
@@ -35,26 +35,95 @@ public class StringMethodPractice {
 //		System.out.println(s.toUpperCase());
 //		System.out.println(s1.toLowerCase());
 
-		String url = "https://www.google.com/";
+//		String url = "https://www.google.com/";
+//
+//		boolean startsWith = url.startsWith("https://");
+//
+//		if (startsWith == true) {
+//			System.out.println("The URL is Secured");
+//		} else {
+//			System.out.println("The URL is not secured");
+//		}
+//
+//		// endsWith()
+//
+//		boolean endsWith1 = url.endsWith(".comm/");
+//		boolean endsWith2 = url.endsWith(".net/");
+//
+//		if (endsWith1 == true || endsWith2 == true) {
+//			System.out.println("The Valid url");
+//		} else
+//			System.out.println("The URL is not valid");
 
-		boolean startsWith = url.startsWith("https://");
+//
 
-		if (startsWith == true) {
-			System.out.println("The URL is Secured");
-		} else {
-			System.out.println("The URL is not secured");
-		}
+//		String s1 = "Hello Wordlllllll";
+//
+//		for (int i = 0; i < s1.length(); i++) {
+//
+//			if(s1.charAt(i)=='H')
+//			{
+//				System.out.println(i);
+//			}
+//			
+//
+//		}
 
-		// endsWith()
+//		System.out.println(s1.length());
+//		
+//		int indexOf = s1.indexOf('l');
+//		
+//		int lastIndexOf = s1.lastIndexOf('l');
+//
+//		System.out.println(indexOf);
+//		
+//		System.out.println(lastIndexOf);
+		
+//		String s2 = "test java hello java";
+//		
+//		boolean contains = s2.contains("hellow");
+//		
+//		System.out.println(contains);
+		
+//		String s1 = "test java";
+//		
+//		String s2 = "TEST JAVA";
+//		
+//		boolean equals = s1.equals(s2);
+//		
+//		boolean equalsIgnoreCase = s1.equalsIgnoreCase(s2);
+//		
+//		System.out.println(equals);
+//		
+//		System.out.println(equalsIgnoreCase);
+		
+		
+	//	String s1 = "Hi Welocome to the room";
+		
+//		String substring = s1.substring(3, 11);
+//		
+//		System.out.println(substring);
+		
+//		String substring = s1.substring(3);
+//		
+//		System.out.println(substring);
 
-		boolean endsWith1 = url.endsWith(".comm/");
-		boolean endsWith2 = url.endsWith(".net/");
-
-		if (endsWith1 == true || endsWith2 == true) {
-			System.out.println("The Valid url");
-		} else
-			System.out.println("The URL is not valid");
-
+		
+//		String s1 = "Welcome to ";
+//		
+//		String s2 = "the JAVA class";
+//		
+//		String concat = s1.concat(s2);
+//		
+//		System.out.println(concat);
+		
+		
+		String s1 = "Test Java";
+		String replace = s1.replace("Test", "Hello");
+		
+		System.out.println(replace);
+		
+		
 	}
 
 }
